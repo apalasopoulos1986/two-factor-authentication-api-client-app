57fa7a2183af2284b216d8c90eac49fb69a40abf
Anastasios Palasopoulos <57755419+apalasopoulos1986@users.noreply.github.com>
2022-04-29 00:33:13 +0300

    Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,32 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { fakeBackendProvider } from './_helpers';
 
 
+import { AppRoutingModule } from './app-routing.module';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
 import { AppComponent } from './app.component';
-import { AlertComponent } from './_components/alert.component';
-import { HomeComponent } from './home/home.component';
-
+import { AlertComponent } from './_components';
+import { HomeComponent } from './home';
 
 
 @NgModule({
-  
-  declarations: [
-    AppComponent,
-    AlertComponent,
-    HomeComponent
-  ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      ReactiveFormsModule,
+      HttpClientModule,
+      AppRoutingModule
   ],
-  providers: [],
+  declarations: [
+      AppComponent,
+      AlertComponent,
+      HomeComponent
+  ],
+  providers: [
+      { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+      { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+      // provider used to create fake backend
+      fakeBackendProvider
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { };
